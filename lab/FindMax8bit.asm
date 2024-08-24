342b3b2ca99a1204a4f914c2420114c9a236b9b6
Sanjeev Subrahmaniyan <subrahmaniyansanjeev@gmail.com>
2024-08-24 12:34:59 +0530

    Added all assembly files for week 2 experiment

---
+++ lab/FindMax8bit.asm
@@ -0,0 +1,35 @@
+;
+; MaxFind8bit.asm
+;
+; Created: 24-08-2024 12:00:48
+; Author : Sanjeev S S B
+;
+
+.CSEG
+LDI ZL, LOW(VALS<<1)
+LDI ZH, HIGH(VALS<<1)
+LDI XL, 0x60
+LDI XH, 0x00
+LDI R16, 0x00; This will contain the maximum of the numbers and is hence initialized to zero
+LDI R20, 0x06; This contains the number of values to iterate over
+
+loop:
+LPM R17, Z+
+CP R17, R16
+BRLO loop2
+MOV R16, R17
+
+loop2:
+DEC R20
+BRNE loop
+RJMP did
+
+did:
+ST X, R16
+RJMP done
+
+done:
+RJMP done
+
+
+VALS: .db 0x12, 0x34, 0xaf, 0x53, 0x64, 0x43, 0x88
---
+++ lab/Multiply2x1Byte.asm
@@ -0,0 +1,22 @@
+;
+; Multiply2Byte.asm
+;
+; Created: 24-08-2024 11:48:46
+; Author : Sanjeev S S B
+;
+
+
+; Replace with your application code
+.CSEG
+LDI ZL, LOW(VALS<<1)
+LDI ZH, HIGH(VALS<<1)
+LPM R16, Z+
+LPM R17, Z
+MUL	R16, R17
+LDI XL, 0x60
+LDI XH, 0x00
+ST X+, R0
+ST X, R1
+NOP 
+
+VALS: .db 0x32, 0x22;
